56cfa9fdff12f0fac18096ed206b5b745d6ed483
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-10-09 10:57:46 +0100

    commit3

--- guide/src/App.jsx
+++ guide/src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Blog from './pages/Blog';
-import Gallery from './pages/Gallery';
-import Reviews from './pages/Reviews';
-import About from './pages/About';
+import Home from './Home';
+
 import Navbar from './composants/navbar';
 import Footer from './composants/footer';
 
@@ -14,10 +11,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/reviews" element={<Reviews />} />
-          <Route path="/about" element={<About />} />
+          
         </Routes>
         <Footer />
       </Router>
